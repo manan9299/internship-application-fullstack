ae1a38243f89d09158934f0e829e9041fe52daa0
Manan Shah <mananshah@Manans-MacBook-Pro.local>
2020-04-13 23:41:57 -0700

    Basic random page redirection implemented

--- manan_shah_random_app/index.js
+++ manan_shah_random_app/index.js
@@ -6,7 +6,26 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+
+  const URL = "https://cfw-takehome.developers.workers.dev/api/variants"
+
+  let urlListResp = await fetch(URL)
+  let urlListData = await urlListResp.json();
+
+  urlListData = urlListData["variants"]
+  console.log(urlListData["variants"])
+
+  let selectedVariantIdx = getRandom(2);
+  console.log(urlListData[selectedVariantIdx])
+
+  let selectedVariantResp = await fetch(urlListData[selectedVariantIdx].toString())
+  let selectedVariantHtml = await selectedVariantResp.text();
+
+  return new Response(selectedVariantHtml, {
+    headers : {"content-type" : "text/html"}
   })
+}
+
+function getRandom(maxNum){
+  return Math.floor(Math.random() * maxNum)
 }
